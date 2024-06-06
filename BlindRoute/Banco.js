ee248ecc8b2251dac068ae4b4ec2fe2400d20b06
Ana2020143716 <anacsrodrigues.46@gmail.com>
2024-06-06 22:11:38 +0100

    +

--- BlindRoute/Banco.js
+++ BlindRoute/Banco.js
@@ -1,40 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, Linking } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import { MaterialIcons } from '@expo/vector-icons';
 
 export default function Banco() {
-  const [banks, setBanks] = useState([]);
-  const [nearestBank, setNearestBank] = useState(null);
-
-  useEffect(() => {
-    fetchBanksNearby(6.8523, 79.8895); // Coordenadas manualmente definidas (latitude e longitude)
-  }, []);
-
-  const fetchBanksNearby = async (latitude, longitude) => {
-    try {
-      const response = await fetch(
-        `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&radius=5000&type=bank&key=YOUR_API_KEY`
-      );
-      const data = await response.json();
-      setBanks(data.results);
-
-      // Encontrar o banco mais próximo
-      if (data.results.length > 0) {
-        const nearest = data.results[0];
-        setNearestBank(nearest);
-      }
-    } catch (error) {
-      console.error('Erro ao buscar bancos:', error);
-    }
-  };
+  const destinationCoords = { latitude: 6.8497, longitude: 79.9502 }; // Coordenadas manuais para o local de destino
 
   const handleGetDirections = () => {
-    if (nearestBank) {
-      const { lat, lng } = nearestBank.geometry.location;
-      const url = `https://www.google.com/maps/dir/?api=1&destination=${lat},${lng}`;
-      Linking.openURL(url);
-    }
+    const { latitude, longitude } = destinationCoords;
+    const url = `https://www.google.com/maps/dir/?api=1&destination=${latitude},${longitude}`;
+    Linking.openURL(url);
   };
 
   return (
@@ -42,22 +17,13 @@ export default function Banco() {
       <MapView
         style={styles.map}
         region={{
-          latitude: 6.8523,
-          longitude: 79.8895,
+          latitude: destinationCoords.latitude,
+          longitude: destinationCoords.longitude,
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
         }}
       >
-        {banks.map((bank, index) => (
-          <Marker
-            key={index}
-            coordinate={{
-              latitude: bank.geometry.location.lat,
-              longitude: bank.geometry.location.lng,
-            }}
-            title={bank.name}
-          />
-        ))}
+        <Marker coordinate={destinationCoords} title="Local de Destino" />
       </MapView>
       <View style={styles.buttonContainer}>
         <TouchableOpacity style={styles.button} onPress={handleGetDirections}>

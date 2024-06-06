3f5fb79886df7ddbe364782d4083d800a9112df8
Ana2020143716 <anacsrodrigues.46@gmail.com>
2024-06-06 14:12:30 +0100

    mapa

--- BlindRoute/Banco.js
+++ BlindRoute/Banco.js
@@ -1,19 +1,21 @@
+import React from 'react';
+import MapView from 'react-native-maps';
+import { StyleSheet, Platform, View } from 'react-native';
 
-import * as React from 'react';
-import { Button, View, Text } from 'react-native';
-
-
-export default function Banco({ navigation }) {
+export default function Banco() {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Banco</Text>
-      <Button
-        title="Go to Details... again"
-      
-        onPress={() => navigation.push('Details')}
-      />
-<Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
-<Button title="Go back" onPress={() => navigation.goBack()} />
+    <View style={styles.container}>
+      <MapView style={styles.map} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+});

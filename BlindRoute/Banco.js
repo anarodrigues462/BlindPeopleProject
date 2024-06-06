876b20075809dc0fe6037853c39e8bf30395943a
Ana2020143716 <anacsrodrigues.46@gmail.com>
2024-06-06 22:12:54 +0100

    +

--- BlindRoute/Banco.js
+++ BlindRoute/Banco.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, Linking } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import { MaterialIcons } from '@expo/vector-icons';
 
 export default function Banco() {
-  const destinationCoords = { latitude: 6.8497, longitude: 79.9502 }; // Coordenadas manuais para o local de destino
+  const [startCoords, setStartCoords] = useState({ latitude: 6.8523, longitude: 79.8895 }); // Coordenadas manuais para o ponto de partida
+  const [endCoords, setEndCoords] = useState({ latitude: 6.8497, longitude: 79.9502 }); // Coordenadas manuais para o ponto de destino
 
   const handleGetDirections = () => {
-    const { latitude, longitude } = destinationCoords;
-    const url = `https://www.google.com/maps/dir/?api=1&destination=${latitude},${longitude}`;
+    const { latitude: startLat, longitude: startLng } = startCoords;
+    const { latitude: endLat, longitude: endLng } = endCoords;
+    const url = `https://www.google.com/maps/dir/?api=1&origin=${startLat},${startLng}&destination=${endLat},${endLng}`;
     Linking.openURL(url);
   };
 
@@ -17,13 +19,14 @@ export default function Banco() {
       <MapView
         style={styles.map}
         region={{
-          latitude: destinationCoords.latitude,
-          longitude: destinationCoords.longitude,
+          latitude: startCoords.latitude,
+          longitude: startCoords.longitude,
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
         }}
       >
-        <Marker coordinate={destinationCoords} title="Local de Destino" />
+        <Marker coordinate={startCoords} title="Ponto de Partida" />
+        <Marker coordinate={endCoords} title="Ponto de Destino" />
       </MapView>
       <View style={styles.buttonContainer}>
         <TouchableOpacity style={styles.button} onPress={handleGetDirections}>

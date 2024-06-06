f47057b6362f574e20abdcdafd1782a513e4697c
Francisco Rodrigues <franciscofrodrigues28@gmail.com>
2024-06-06 17:00:53 +0100

    Update App.js

--- BlindRoute/App.js
+++ BlindRoute/App.js
@@ -1,12 +1,11 @@
 
 import * as React from 'react';
-import { Button, View, Text } from 'react-native';
+import { StyleSheet, Button, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './HomeScreen';
 import RotasPreferidas from './RotasPreferidas';
 import Banco from './Banco';
-
 
 const Stack = createNativeStackNavigator();
 
@@ -15,7 +14,7 @@ function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="BlindRoute" component={HomeScreen} />
-        <Stack.Screen name="Rotas Preferidas" component={RotasPreferidas} />
+        <Stack.Screen name="Rotas Preferidas" component={RotasPreferidas} />    
         <Stack.Screen name="Banco" component={Banco} />
       </Stack.Navigator>
     </NavigationContainer>
